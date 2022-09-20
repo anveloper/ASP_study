2fed5340edda7d5648110b885cd08e6333ea1f4d
anveloper <hitedin@gmail.com>
2022-09-21 06:50:35 +0900

    sol: day226 BOJ1475

---
+++ day249/Day226BOJ1475방번호.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string str;
+  cin >> str;
+
+  int arr[10] = {0};
+  int tmp;
+  for (int i = 0; i < str.size(); i++) {
+    tmp = str[i] - '0';
+    if (tmp == 9) {
+      arr[6]++;
+    } else {
+      arr[tmp]++;
+    }
+  }
+
+  int max = 1;
+
+  if (arr[6] % 2 == 0) {
+    arr[6] = ceil(arr[6] / 2);
+  } else {
+    arr[6] = ceil(arr[6] / 2) + 1;
+  }
+
+  for (int i = 0; i < 9; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+
+  cout << max;
+
+  return 0;
+}
