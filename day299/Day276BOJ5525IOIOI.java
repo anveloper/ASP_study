69b75311c4def3f573dc2fd5e1a58a05d7e6d2d1
anveloper <hitedin@gmail.com>
2022-11-10 22:12:16 +0900

    sol: day276 BOJ5525

---
+++ day299/Day276BOJ5525IOIOI.java
@@ -0,0 +1,33 @@
+package ASP_study.day299;
+
+import java.io.*;
+
+public class Day276BOJ5525IOIOI {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		char s[] = br.readLine().toCharArray();
+
+		int result = 0;
+		int count = 0;
+
+		for (int i = 1; i < M - 1; i++) {
+			if (s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+				count++;
+
+				if (count == N) {
+					count--;
+					result++;
+				}
+				i++;
+			} else {
+				count = 0;
+			}
+		}
+
+		System.out.println(result);
+		br.close();
+	}
+}
