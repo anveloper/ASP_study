2f0a2057861522709dace208717bc191bed1c66b
anveloper <hitedin@gmail.com>
2022-11-30 06:43:36 +0900

    sol: day296 BOJ1449

---
+++ day299/Day296BOJ1449수리공정렬.java
@@ -0,0 +1,31 @@
+package ASP_study.day299;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Day296BOJ1449수리공정렬 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int water[] = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			water[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(water);
+		double left = water[0] - 0.5;
+		int cnt = 1;
+		for (int i = 0; i < N; i++) {
+			if (left + L < water[i]) {
+				cnt++;
+				left = water[i] - 0.5;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
