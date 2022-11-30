4908b4a1318dc5b994500ea36abe73756c9b986e
anveloper <hitedin@gmail.com>
2022-12-01 07:09:35 +0900

    sol: day297 BOJ1550

---
+++ day299/Day297BOJ155016진수.java
@@ -0,0 +1,7 @@
+package ASP_study.day299;
+
+class Day297BOJ155016진수 {
+	public static void main(String[] z) {
+		System.out.println(Integer.parseInt(new java.util.Scanner(System.in).next(), 16));
+	}
+}
