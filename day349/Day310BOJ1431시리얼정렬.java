8a24d4d42005306d0e0f8ded40f3d8643c382b71
anveloper <hitedin@gmail.com>
2022-12-14 07:45:23 +0900

    sol: day310 BOJ1431

---
+++ day349/Day310BOJ1431시리얼정렬.java
@@ -0,0 +1,50 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Day310BOJ1431시리얼정렬 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() < o2.length()) {
+                    return -1;
+                } else if (o1.length() == o2.length()) {
+                    if (getSum(o1) == getSum(o2)) {
+                        return o1.compareTo(o2);
+                    } else {
+                        return Integer.compare(getSum(o1), getSum(o2));
+                    }
+                } else {
+                    return 1;
+                }
+
+            }
+        });
+
+        for (String i : arr) {
+            System.out.println(i);
+        }
+
+    }
+
+    public static int getSum(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                sum += s.charAt(i) - '0';
+            }
+        }
+        return sum;
+    }
+}
