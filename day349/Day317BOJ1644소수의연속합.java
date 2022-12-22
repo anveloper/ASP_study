9da040f8b2ce6511bfdbe80e6a9e433bb2e9fe3a
anveloper <hitedin@gmail.com>
2022-12-23 08:56:46 +0900

    sol: day317 BOJ1644

--- day349/Day317BOJ1644소수의연속합.java
+++ day349/Day317BOJ1644소수의연속합.java
@@ -1,3 +1,5 @@
+package day349;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -13,18 +15,25 @@ public class Day317BOJ1644소수의연속합 {
         prime = new boolean[N + 1];
         prime[0] = prime[1] = true;
         for (int i = 2; i * i <= N; i++) {
-            if (!prime[i]) for (int j = i * i; j <= N; j += i) prime[j] = true;
+            if (!prime[i])
+                for (int j = i * i; j <= N; j += i)
+                    prime[j] = true;
         }
         for (int i = 1; i <= N; i++) {
-            if (!prime[i]) primes.add(i);
+            if (!prime[i])
+                primes.add(i);
         }
 
         int l = 0, r = 0, sum = 0, cnt = 0;
         while (true) {
-            if (sum >= N) sum -= primes.get(l++);
-            else if (r == primes.size()) break;
-            else sum += primes.get(r++);
-            if (N == sum) cnt++;
+            if (sum >= N)
+                sum -= primes.get(l++);
+            else if (r == primes.size())
+                break;
+            else
+                sum += primes.get(r++);
+            if (N == sum)
+                cnt++;
         }
         System.out.println(cnt);
         br.close();
