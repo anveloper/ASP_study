f15047f1c603890936e6d65ab81f2a221768b2d2
anveloper <hitedin@gmail.com>
2022-12-31 11:26:16 +0900

    sol: day327 BOJ15311

---
+++ day349/Day327BOJ15311약팔기.java
@@ -0,0 +1,15 @@
+package day349;
+
+public class Day327BOJ15311약팔기 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("2000\n");
+        for (int i = 0; i < 1000; i++) {
+            sb.append("1 ");
+        }
+        for (int i = 0; i < 1000; i++) {
+            sb.append("1000 ");
+        }
+        System.out.println(sb);
+    }
+}
