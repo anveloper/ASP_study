db87a0f8ade16e7da711613508aed7cbe8456e9c
anveloper <hitedin@gmail.com>
2023-01-26 08:45:50 +0900

    sol: day353 BOJ10817

---
+++ day399/Day353BOJ10817세수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Day353BOJ10817세수 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int[] arr = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = i + 1; j < 3; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(arr[1]);
+    }
+}
