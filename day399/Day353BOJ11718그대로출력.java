d5841f0562a7cbe2841ba1d0b421e72f92f6c5d4
anveloper <hitedin@gmail.com>
2023-01-26 08:46:04 +0900

    sol: day353 BOJ11718

---
+++ day399/Day353BOJ11718그대로출력.java
@@ -0,0 +1,12 @@
+
+import java.io.*;
+
+public class Day353BOJ11718그대로출력 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+}
