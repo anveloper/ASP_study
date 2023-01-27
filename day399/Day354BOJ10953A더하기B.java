cf090180fdf7c41c4955567d48a31a1941f4af45
anveloper <hitedin@gmail.com>
2023-01-27 09:41:51 +0900

    sol: day354 BOJ10953

---
+++ day399/Day354BOJ10953A더하기B.java
@@ -0,0 +1,16 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Day354BOJ10953A더하기B {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test_case = Integer.parseInt(br.readLine());
+		for (int i = 0; i < test_case; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			System.out.println(a + b);
+		}
+	}
+}
