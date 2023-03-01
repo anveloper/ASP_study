0836833ea93fe9de01d97e8dadeb20f71d49afb3
anveloper <hitedin@gmail.com>
2023-03-02 07:11:00 +0900

    sol: day388 BOJ1000

---
+++ day399/Day388BOJ1000덧셈.java
@@ -0,0 +1,11 @@
+package day399;
+
+import java.util.*;
+
+public class Day388BOJ1000덧셈 { // ㅋㅋ
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println(sc.nextInt() + sc.nextInt());
+    sc.close();
+  }
+}
