f53b07c68a9e77a244b9234d7e5308af24385c04
anveloper <hitedin@gmail.com>
2023-03-20 07:03:22 +0900

    sol: day407 BOJ1402

---
+++ day449/Day407BOJ1402A번난이도.java
@@ -0,0 +1,15 @@
+package day449;
+
+import java.io.*;
+
+public class Day407BOJ1402A번난이도 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int t = Integer.parseInt(br.readLine());
+    while (t-- > 0) {
+      sb.append("yes").append('\n');
+    }
+    System.out.print(sb);
+  }
+}
