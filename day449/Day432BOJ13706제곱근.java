a92ce9d29a300770159ce800a11b9634f32c7f1e
anveloper <hitedin@gmail.com>
2023-04-14 06:54:58 +0900

    sol: day432 BOJ 13706

---
+++ day449/Day432BOJ13706제곱근.java
@@ -0,0 +1,10 @@
+package day449;
+
+import java.io.*;
+import java.math.*;
+
+public class Day432BOJ13706제곱근 {
+  public static void main(String[] args) throws Exception {
+    System.out.println(new BigInteger(new BufferedReader(new InputStreamReader(System.in)).readLine()).sqrt());
+  }
+} // BigInteger.sqrt() 함수 Java8버전은 불가능
