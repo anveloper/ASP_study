3e29d267ab2aaf7150bb31cd14190a2b6081dbc4
anveloper <hitedin@gmail.com>
2023-06-08 06:35:53 +0900

    sol: day487 BOJ10998

---
+++ day499/Day487BOJ10998AxB.java
@@ -0,0 +1,12 @@
+package day499;
+
+import java.io.*;
+import java.util.*;
+
+public class Day487BOJ10998AxB {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    System.out.println(Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken()));
+  }
+}
