931e30a06064723d8ec50f6dbfdd8b74bc12194c
anveloper <hitedin@gmail.com>
2023-07-10 06:40:10 +0900

    sol: day519 BOJ2558

---
+++ day549/Day519BOJ2558ab2.java
@@ -0,0 +1,14 @@
+package day549;
+
+import java.util.Scanner;
+
+public class Day519BOJ2558ab2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    sc.close();
+
+    System.out.println(a + b);
+  }
+}
