c866b4c569d090a8a71cbd82aad2ecc34fbe5dd4
anveloper <hitedin@gmail.com>
2023-07-11 06:30:22 +0900

    sol: day520 BOJ2775

---
+++ day549/Day520BOJ2775부녀회장.java
@@ -0,0 +1,32 @@
+package day549;
+
+import java.io.*;
+
+public class Day520BOJ2775부녀회장 {
+  public static int[][] APT = new int[15][15];
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < 15; i++) {
+      APT[i][1] = 1;
+      APT[0][i] = i;
+    }
+
+    for (int i = 1; i < 15; i++) {
+      for (int j = 2; j < 15; j++) {
+        APT[i][j] = APT[i][j - 1] + APT[i - 1][j];
+      }
+    }
+
+    int T = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < T; i++) {
+      int k = Integer.parseInt(br.readLine());
+      int n = Integer.parseInt(br.readLine());
+      sb.append(APT[k][n]).append('\n');
+    }
+    System.out.println(sb);
+  }
+}
