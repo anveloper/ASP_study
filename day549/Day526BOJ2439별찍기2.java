54be4649ebbc7565fce77d33967bd3d73367648c
anveloper <hitedin@gmail.com>
2023-07-17 07:15:55 +0900

    sol: day526 BOJ2439

---
+++ day549/Day526BOJ2439별찍기2.java
@@ -0,0 +1,25 @@
+package day549;
+
+import java.io.*;
+
+public class Day526BOJ2439별찍기2 {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int N = Integer.parseInt(br.readLine());
+    br.close();
+
+    for (int i = 1; i <= N; i++) {
+      for (int j = 1; j <= N - i; j++) {
+        bw.write(' ');
+      }
+      for (int k = 1; k <= i; k++) {
+        bw.write('*');
+      }
+      bw.newLine();
+    }
+    bw.flush();
+    bw.close();
+  }
+}
