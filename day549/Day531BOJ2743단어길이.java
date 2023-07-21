4ef6e1f4899a304c0e3a86f0b4b48c1ee1637500
anveloper <hitedin@gmail.com>
2023-07-22 08:31:59 +0900

    sol: day531 BOJ2743

---
+++ day549/Day531BOJ2743단어길이.java
@@ -0,0 +1,11 @@
+package day549;
+
+import java.util.Scanner;
+
+public class Day531BOJ2743단어길이 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println(sc.nextLine().length());
+    sc.close();
+  }
+}
