ccc9cf0242bd9ca234b5a395eaaf0d9dd448bbd5
anveloper <hitedin@gmail.com>
2023-08-23 06:33:01 +0900

    sol: day563 BOJ1259

---
+++ day599/Day563BOJ1259펠린드롬수.java
@@ -0,0 +1,27 @@
+package day599;
+
+import java.io.*;
+
+public class Day563BOJ1259펠린드롬수 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    while (true) {
+      String str = br.readLine();
+      boolean chk = true;
+
+      if (str.equals("0"))
+        break;
+
+      for (int i = 0; i < str.length() / 2; i++) {
+        if (str.charAt(i) != str.charAt(str.length() - 1 - i))
+          chk = false;
+      }
+
+      if (chk)
+        System.out.println("yes");
+      else
+        System.out.println("no");
+    }
+  }
+}
