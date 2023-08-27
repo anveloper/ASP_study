82c14ada053d13daf15db7d343b2f9d5792d59e4
anveloper <hitedin@gmail.com>
2023-08-28 06:33:41 +0900

    sol: day570 BOJ2745

---
+++ day599/Day570BOJ2745진법변환.java
@@ -0,0 +1,31 @@
+package day599;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Day570BOJ2745진법변환 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    String N = st.nextToken();
+    int B = Integer.parseInt(st.nextToken());
+    br.close();
+
+    int tmp = 1;
+    int sum = 0;
+
+    for (int i = N.length() - 1; i >= 0; i--) {
+      char C = N.charAt(i);
+
+      if ('A' <= C && C <= 'Z') {
+        sum += (C - 'A' + 10) * tmp;
+      } else {
+        sum += (C - '0') * tmp;
+      }
+      tmp *= B;
+    }
+
+    System.out.println(sum);
+  }
+}
