d5142ab7138cecb9800b9a66dce168f975e53957
anveloper <hitedin@gmail.com>
2023-09-10 09:15:57 +0900

    sol: day583 BOJ3046

---
+++ day599/Day583BOJ3046R2.java
@@ -0,0 +1,13 @@
+package day599;
+
+import java.util.Scanner;
+
+public class Day583BOJ3046R2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println((b * 2) - a);
+    sc.close();
+  }
+}
