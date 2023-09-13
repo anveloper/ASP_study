f5984bbff58575f4b4c81f7427d7eefe74151afe
anveloper <hitedin@gmail.com>
2023-09-14 06:56:45 +0900

    sol: day587 BOJ4948

---
+++ day599/Day587BOJ4948베르트랑공준.java
@@ -0,0 +1,40 @@
+package day599;
+
+import java.io.*;
+
+public class Day587BOJ4948베르트랑공준 {
+  public static boolean[] prime = new boolean[246913];
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    get_prime();
+
+    while (true) {
+      int n = Integer.parseInt(br.readLine());
+      if (n == 0)
+        break;
+
+      int count = 0;
+
+      for (int i = n + 1; i <= 2 * n; i++) {
+        if (!prime[i])
+          count++;
+      }
+      System.out.println(count);
+    }
+  }
+
+  public static void get_prime() {
+
+    prime[0] = prime[1] = true;
+
+    for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+      if (prime[i])
+        continue;
+      for (int j = i * i; j < prime.length; j += i) {
+        prime[j] = true;
+      }
+    }
+  }
+}
