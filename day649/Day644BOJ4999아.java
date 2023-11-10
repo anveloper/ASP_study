c671dbe4136d0d6ec7b074e6dbd5519ce8fa331f
anveloper <hitedin@gmail.com>
2023-11-11 08:09:21 +0900

    sol: day644 BOJ4999

---
+++ day649/Day644BOJ4999아.java
@@ -0,0 +1,10 @@
+package day649;
+
+import java.io.*;
+
+public class Day644BOJ4999아 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println((br.readLine().length() >= br.readLine().length()) ? "go" : "no");
+  }
+}
