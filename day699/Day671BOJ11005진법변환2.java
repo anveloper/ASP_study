32f3e1fdbf3536fa51581a4265164e2bec82f6f3
anveloper <hitedin@gmail.com>
2023-12-08 06:45:39 +0900

    sol: day671 BOJ11005

--- day699/Day671BOJ11005진법변환2.java
+++ day699/Day671BOJ11005진법변환2.java
@@ -1,29 +1,13 @@
 package day699;
 
 import java.io.*;
-import java.util.*;
 
 public class Day671BOJ11005진법변환2 {
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    StringBuilder sb = new StringBuilder();
-    int n = Integer.parseInt(st.nextToken());
-    int b = Integer.parseInt(st.nextToken());
-
-    List<Character> list = new ArrayList<>();
-    while (n > 0) {
-      if (n % b < 10) {
-        list.add((char) (n % b + '0'));
-      } else {
-        list.add((char) (n % b - 10 + 'A'));
-      }
-      n /= b;
-    }
-
-    for (int i = list.size() - 1; i >= 0; i--) {
-      sb.append(list.get(i));
-    }
-    System.out.println(sb);
+    String[] input = br.readLine().split(" ");
+    int n = Integer.parseInt(input[0]);
+    int b = Integer.parseInt(input[1]);
+    System.out.println(Integer.toString(n, b).toUpperCase());
   }
 }
