898aa4b6f6a6918c1ff78c1b1c330a609e500ae0
anveloper <hitedin@gmail.com>
2023-12-12 06:21:43 +0900

    sol: day675 BOJ5338

---
+++ day699/Day675BOJ5338마이크로소프트로고.java
@@ -0,0 +1,13 @@
+package day699;
+
+public class Day675BOJ5338마이크로소프트로고 {
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("       _.-;;-._\n"
+        + "'-..-'|   ||   |\n"
+        + "'-..-'|_.-;;-._|\n"
+        + "'-..-'|   ||   |\n"
+        + "'-..-'|_.-''-._|");
+    System.out.println(sb);
+  }
+}
