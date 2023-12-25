e1ab5079194a03ed241692a50d8d94ac485925ec
anveloper <hitedin@gmail.com>
2023-12-26 06:33:57 +0900

    sol: day693 BOJ10172

---
+++ day699/Day693BOJ10172개.java
@@ -0,0 +1,7 @@
+package day699;
+
+public class Day693BOJ10172개 {
+  public static void main(String[] args) {
+    System.out.print("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+  }
+}
