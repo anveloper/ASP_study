82445ce941b043297472b866f4e87b38f7bf31c1
anveloper <hitedin@gmail.com>
2023-12-31 08:12:34 +0900

    sol: day698 BOJ5337

---
+++ day699/Day698BOJ5337웰컴.java
@@ -0,0 +1,7 @@
+package day699;
+
+public class Day698BOJ5337웰컴 {
+  public static void main(String[] args) {
+    System.out.println(".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.");
+  }
+}
