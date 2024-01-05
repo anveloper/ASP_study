0f395b9149b7646fe9667db7687f451aeb538160
anveloper <hitedin@gmail.com>
2024-01-06 08:44:56 +0900

    sol: day704 BOJ2666

---
+++ day749/Day704BOJ2666벽장문의이동.java
@@ -0,0 +1,43 @@
+package day749;
+
+import java.io.*;
+import java.util.*;
+
+public class Day704BOJ2666벽장문의이동 {
+  static List<Integer> opens;
+  static int[] target;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    Integer.parseInt(br.readLine());
+
+    String[] line = br.readLine().split(" ");
+    opens = new ArrayList<>();
+    for (int i = 0; i < line.length; i++) {
+      opens.add(Integer.parseInt(line[i]));
+    }
+
+    int test = Integer.parseInt(br.readLine());
+
+    target = new int[test];
+    for (int i = 0; i < test; i++) {
+      target[i] = Integer.parseInt(br.readLine());
+    }
+    System.out.println(solve(0, opens.get(0), opens.get(1)));
+
+  }
+
+  static int solve(int cnt, int a, int b) {
+
+    if (cnt == target.length)
+      return 0;
+
+    int tmp1 = Math.abs(a - target[cnt]);
+    int tmp2 = Math.abs(b - target[cnt]);
+
+    return Math.min(tmp1 + solve(cnt + 1, b, target[cnt]),
+        tmp2 + solve(cnt + 1, a, target[cnt]));
+
+  }
+}
