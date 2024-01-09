0dc8cff52e3b538ecfa2efb85d5337f266ee92bb
anveloper <hitedin@gmail.com>
2024-01-09 09:16:11 +0900

    sol: day707 BOJ10102

--- day749/Day707BOJ10102개표.java
+++ day749/Day707BOJ10102개표.java
@@ -5,16 +5,14 @@ import java.io.*;
 public class Day707BOJ10102개표 {
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    Integer.parseInt(br.readLine());
+    br.readLine();
     int a = 0, b = 0;
-    String str = br.readLine().toLowerCase();
-    for (int i = 0; i < str.length(); i++) {
-      if (str.charAt(i) == 'a')
+    for (char c : br.readLine().toLowerCase().toCharArray()) {
+      if (c == 'a')
         a++;
       else
         b++;
     }
-
     System.out.println((a > b) ? "A" : (a < b) ? "B" : "Tie");
   }
 }
