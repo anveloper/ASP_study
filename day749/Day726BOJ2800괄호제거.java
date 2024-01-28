1e8c4e919854b3d4b8fca511628767fe8e80963e
anveloper <hitedin@gmail.com>
2024-01-28 20:11:52 +0900

    sol: day726 BOJ2800

---
+++ day749/Day726BOJ2800괄호제거.java
@@ -0,0 +1,69 @@
+package day749;
+
+import java.util.*;
+import java.io.*;
+
+public class Day726BOJ2800괄호제거 {
+  static char[] ch;
+  static Stack<Integer> st;
+
+  static List<int[]> list = new ArrayList<>();
+  static boolean[] visited;
+
+  static Set<String> result = new TreeSet<>();
+
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    ch = br.readLine().toCharArray();
+
+    st = new Stack<>();
+
+    for (int i = 0; i < ch.length; i++) {
+      if (ch[i] == '(') {
+        st.push(i);
+      } else if (ch[i] == ')') {
+        list.add(new int[] { st.pop(), i });
+      }
+    }
+
+    visited = new boolean[ch.length];
+    comb(0);
+
+    for (String s : result) {
+      sb.append(s).append("\n");
+    }
+    System.out.println(sb);
+  }
+
+  private static void comb(int depth) {
+    if (depth == list.size()) {
+      StringBuilder tmp = new StringBuilder();
+      boolean b = false;
+
+      for (int i = 0; i < ch.length; i++) {
+        if (!visited[i]) {
+          tmp.append(ch[i]);
+        } else {
+          b = true;
+        }
+      }
+
+      if (b) {
+        result.add(tmp.toString());
+      }
+
+      return;
+    }
+
+    comb(depth + 1);
+
+    int[] now = list.get(depth);
+    visited[now[0]] = true;
+    visited[now[1]] = true;
+    comb(depth + 1);
+    visited[now[0]] = false;
+    visited[now[1]] = false;
+  }
+}
