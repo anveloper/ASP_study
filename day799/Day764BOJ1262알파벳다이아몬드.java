20e34336cca9f5d8a168a094869b9134f62d81f2
anveloper <hitedin@gmail.com>
2024-03-06 06:40:25 +0900

    sol: day764 BOJ1262

---
+++ day799/Day764BOJ1262알파벳다이아몬드.java
@@ -0,0 +1,32 @@
+package day799;
+
+import java.util.Scanner;
+
+public class Day764BOJ1262알파벳다이아몬드 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String[] input = sc.nextLine().split(" ");
+    int N = Integer.parseInt(input[0]);
+    int R1 = Integer.parseInt(input[1]);
+    int C1 = Integer.parseInt(input[2]);
+    int R2 = Integer.parseInt(input[3]);
+    int C2 = Integer.parseInt(input[4]);
+    int length = 2 * N - 1;
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0, s = R1; i < R2 - R1 + 1; i++, s++) {
+      for (int j = 0, e = C1; j < C2 - C1 + 1; j++, e++) {
+        int x = s % (length);
+        int y = e % (length);
+        int dis = Math.abs((N - 1) - x) + Math.abs((N - 1) - y);
+
+        if (dis >= N)
+          sb.append(".");
+        else
+          sb.append((char) (((dis % 26)) + 'a'));
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb);
+    sc.close();
+  }
+}
